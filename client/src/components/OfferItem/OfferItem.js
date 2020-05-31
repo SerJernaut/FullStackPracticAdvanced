bf461bca4e740e4f75c2cbb53f88201957289093
serjernaut <serjernaut@yandex.ru>
2020-05-31 13:59:40 +0300

    Completed second FullStack-task: Created LightBox for Image that opens on image click:  state isOpen for component. Created functional (buttons and functions that used functions from props) to resolve/reject offers with alert-confirming. Created separated generator of confirm alert options. Added resolve/reject functions and isFetching to propTypes. Ended development of ModeratorDashboard and OfferItem layouts. Used here custom mixins from moderatorMixins and moderatorVariables files.

--- client/src/components/OfferItem/OfferItem.js
+++ client/src/components/OfferItem/OfferItem.js
@@ -1,17 +1,22 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import styles from './OfferItem.module.sass';
 import CONSTANTS from '../../constants';
 import classNames from "classnames";
 import moment from 'moment';
+import {confirmAlert} from "react-confirm-alert";
+import {generateConfirmAlertOptions} from "../../utils/generateConfirmAlertOptions";
+import LightBox from "react-image-lightbox";
 
-const OfferItem = ({
-                       id, text, fileName, moderationStatus, createdAt, Contest: {
-        contestType, title, User: {
-            userId, displayName
-        }
-    }
-                   }) => {
+const OfferItem = ({id, text, fileName, moderationStatus, createdAt, Contest: {contestType, title, User: {id: userId, displayName}}, resolveOfferByModerator, rejectOfferByModerator, isFetching}) => {
+
+    const resolveOfferByModeratorWithId = () => resolveOfferByModerator({offerId: id});
+
+    const rejectOfferByModeratorWithId = () => rejectOfferByModerator({offerId: id});
+
+    const confirmResolvingOffer = () => confirmAlert(generateConfirmAlertOptions('resolving','resolve', resolveOfferByModeratorWithId, true, true));
+
+    const confirmRejectingOffer = () => confirmAlert(generateConfirmAlertOptions('rejecting','reject', rejectOfferByModeratorWithId, true, true));
 
     const timer = () => {
         const diffBetweenCurrentTimeAndCreatedAtInMs = moment().diff(createdAt);
@@ -25,6 +30,7 @@ const OfferItem = ({
     }
 
     const [timerObj, setTimerUnits] = useState(timer());
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const timeIntervalId = setInterval(() => {
@@ -42,29 +48,41 @@ const OfferItem = ({
                 <span>Offer text: </span><span
                 className={styles.offerItalicItem}>{text ? `${text}` : 'This offer contains only photo'}</span>
             </div>
-            <div className={styles.offerPhotoAndModerationStatusContainer}>
-                <div className={styles.offerPhotoContainer}>
-                    <span className={styles.offerStrongItem}>Offer photo: </span>{fileName ?
-                    <img src={`${CONSTANTS.publicURL}${fileName}`} alt="offerFile"/> :
-                    <span className={styles.offerItalicItem}>This offer contains only text</span>}
+            <div className={styles.offerPhotoAndModerationContainer}>
+                <div className={styles.mainInfoContainer}>
+                    <div className={styles.offerPhotoContainer}>
+                        <span className={styles.offerStrongItem}>Offer photo</span>{fileName ?
+                        <>
+                        <img src={`${CONSTANTS.publicURL}${fileName}`} onClick={()=> setIsOpen(true)} alt="offerFile"/>
+                        {isOpen && <LightBox
+                            mainSrc={`${CONSTANTS.publicURL}${fileName}`}
+                            onCloseRequest={() => setIsOpen(false)}/>}
+                        </>
+                        :
+                        <span className={styles.offerItalicItem}>This offer contains only text</span>}
+                    </div>
+                    <div className={styles.offerAndUserInfo}>
+                        <p className={styles.offerOrUserInfo}>Offer created</p>
+                        <span>{timerObj.d} days {timerObj.h} hours {timerObj.m} minutes {timerObj.s} seconds ago</span>
+                        <p className={styles.offerOrUserInfo}>By User</p><span
+                        className={styles.offerItalicItem}>{displayName} #{userId}</span>
+                    </div>
+                    <div className={styles.contestInfoContainer}>
+                        <p className={styles.offerStrongItem}>Offer created for contest with type:</p>
+                        <span className={styles.offerItalicItem}>{contestType}</span>
+                        <p className={styles.offerStrongItem}>Contest title:</p>
+                        <span className={styles.offerItalicItem}>{title}</span>
+                    </div>
                 </div>
-                <div className={styles.offerAndUserInfo}>
-                    <p className={styles.offerOrUserInfo}>Offer created</p>
-                    <span>{timerObj.d} days {timerObj.h} hours {timerObj.m} minutes {timerObj.s} seconds ago</span>
-                    <p className={styles.offerOrUserInfo}>By User</p><span
-                    className={styles.offerItalicItem}>{displayName} #{userId}</span>
-                </div>
-                <div className={styles.moderationStatusContainer}>
+                <div className={styles.moderationContainer}>
                     <span className={styles.offerStrongItem}>moderator!!!</span>
                     <p className={styles.offerStrongItem}>Offer moderation status is</p><span
                     className={classNames(styles.offerStrongItem, styles.moderationStatus)}>{moderationStatus}</span>
                     <p className={styles.offerItalicItem}>Resolve or reject the offer, please ^_^</p>
-                </div>
-                <div className={styles.contestInfoContainer}>
-                    <p className={styles.offerStrongItem}>Offer created for contest with type:</p>
-                    <span className={styles.offerItalicItem}>{contestType}</span>
-                    <p className={styles.offerStrongItem}>Contest title:</p>
-                    <span className={styles.offerItalicItem}>{title}</span>
+                    <div className={styles.buttonsContainer}>
+                        <button className={classNames(styles.btn, styles.resolvingBtn, {[styles.disabled]: isFetching})} onClick={confirmResolvingOffer} disabled={isFetching}>resolve</button>
+                        <button className={classNames(styles.btn, styles.rejectingBtn, {[styles.disabled]: isFetching})} onClick={confirmRejectingOffer}  disabled={isFetching}>reject</button>
+                    </div>
                 </div>
             </div>
         </li>
@@ -77,6 +95,9 @@ OfferItem.propTypes = {
     fileName: PropTypes.string,
     moderationStatus: PropTypes.string.isRequired,
     createdAt: PropTypes.string.isRequired,
+    resolveOfferByModerator: PropTypes.func.isRequired,
+    rejectOfferByModerator: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired,
     Contest: PropTypes.shape({
         contestType: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
