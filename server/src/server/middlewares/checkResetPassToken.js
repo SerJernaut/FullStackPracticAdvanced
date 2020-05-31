df18ff5483ad3dd8d4a63a08a1a900ea49bb4982
serjernaut <serjernaut@yandex.ru>
2020-05-31 14:15:56 +0300

    Refactored checkResetPassToken.js

--- server/src/server/middlewares/checkResetPassToken.js
+++ server/src/server/middlewares/checkResetPassToken.js
@@ -3,14 +3,13 @@ const CONSTANTS = require('../../constants');
 const TokenError = require('../errors/TokenError')
 
 module.exports = async (req, res, next) => {
-    const {accessToken} = req.body;
-    if (!accessToken) {
-        return next(new TokenError('need token'));
-    }
     try {
-        req.tokenData = jwt.verify(accessToken, CONSTANTS.JWT_SECRET);
-        console.log(req.tokenData)
-        return next();
+        const {accessToken} = req.body;
+        if (accessToken) {
+            req.tokenData = jwt.verify(accessToken, CONSTANTS.JWT_SECRET);
+            return next()
+        }
+        next(new TokenError('need token'));
     } catch (err) {
         next(new TokenError());
     }
