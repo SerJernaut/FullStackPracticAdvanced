7eb5b28cf8027d4424127ae0b714ff98b8c07ba1
serjernaut <serjernaut@yandex.ru>
2020-06-04 13:23:32 +0300

    Refactored LinksSection and added adaptive for medium screens.
    Added new variable $lists-common-padding.
    Created 2 wrappers for listsContainers because of medium screen adaptive layout. Without wrappers need to use very often nth-child and it's hard to select needed elements (so big nesting that affects on site performance).

--- client/src/components/LinksSection/LinksSection.js
+++ client/src/components/LinksSection/LinksSection.js
@@ -4,6 +4,7 @@ import styles from "./LinksSection.module.sass";
 const LinksSection = () => {
     return (
         <section className={styles.links}>
+            <div className={styles.firstListsWrapper}>
                 <div className={styles.listsContainer}>
                     <article className={styles.linksWrapper}>
                         <h3>
@@ -64,122 +65,125 @@ const LinksSection = () => {
                         </article>
                     </div>
                 </div>
-                <h3 className={styles.featuredHeader}>FEATURED CATEGORIES</h3>
-                <div className={styles.listsContainer}>
-                    <article className={styles.linksWrapper}>
-                        <ul className={styles.linksContainer}>
-                            <li><a href="#">Agency &amp; Consulting</a></li>
-                            <li><a href="#">Analytics</a></li>
-                            <li><a href="#">Automotive</a></li>
-                            <li><a href="#">Bar &amp; Brewery</a></li>
-                            <li><a href="#">Beauty &amp; Cosmetics</a></li>
-                            <li><a href="#">Beer, Wine &amp; Spirits</a></li>
-                            <li><a href="#">Bikes Brand</a></li>
-                            <li><a href="#">Biotech</a></li>
-                            <li><a href="#">Bots &amp; AI</a></li>
-                            <li><a href="#">Cannabis &amp; CBD</a></li>
-                            <li><a href="#">Catering</a></li>
-                        </ul>
-                    </article>
-                    <article className={styles.linksWrapper}>
-                        <ul className={styles.linksContainer}>
-                            <li><a href="#">Cleaning</a></li>
-                            <li><a href="#">Co-Working Space Names</a></li>
-                            <li><a href="#">Coffee</a></li>
-                            <li><a href="#">Construction &amp; Architecture</a></li>
-                            <li><a href="#">Crowdfunding</a></li>
-                            <li><a href="#">Cryptocurrency, Blockchain</a></li>
-                            <li><a href="#">Dating &amp; Relationship</a></li>
-                            <li><a href="#">Daycare</a></li>
-                            <li><a href="#">Dental</a></li>
-                            <li><a href="#">Drone</a></li>
-                            <li><a href="#">E-Commerce &amp; Retail</a></li>
-                        </ul>
-                    </article>
-                    <article className={styles.linksWrapper}>
-                        <ul className={styles.linksContainer}>
-                            <li><a href="#">Education &amp; Training</a></li>
-                            <li><a href="#">Entertainment &amp; Arts</a></li>
-                            <li><a href="#">Event Planning &amp; Services</a></li>
-                            <li><a href="#">Events &amp; Conferences</a></li>
-                            <li><a href="#">Fashion &amp; Clothing</a></li>
-                            <li><a href="#">Finance</a></li>
-                            <li><a href="#">Fintech (Finance Technology)</a></li>
-                            <li><a href="#">Fitness &amp; Gym</a></li>
-                            <li><a href="#">Food &amp; Drink</a></li>
-                            <li><a href="#">Food Delivery &amp; Meal Kits</a></li>
-                            <li><a href="#">Food Truck</a></li>
-                        </ul>
-                    </article>
-                    <article className={styles.linksWrapper}>
-                        <ul className={styles.linksContainer}>
-                            <li><a href="#">Footwear</a></li>
-                            <li><a href="#">Furniture &amp; Home Furnishings</a></li>
-                            <li><a href="#">Games &amp; Recreational</a></li>
-                            <li><a href="#">Gaming</a></li>
-                            <li><a href="#">Green &amp; Organic</a></li>
-                            <li><a href="#">Health &amp; Wellness</a></li>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Home &amp; Garden</a></li>
-                            <li><a href="#">Insurance</a></li>
-                            <li><a href="#">Interior Design</a></li>
-                            <li><a href="#">Internet of Things (IOT)</a></li>
-                            <li><a href="#">Jewelry</a></li>
-                            <li><a href="#">Kids &amp; Baby</a></li>
-                            <li><a href="#">Landscaping</a></li>
-                            <li><a href="#">Legal, Attorney, Law</a></li>
-                            <li><a href="#">Life Coach / Motivational</a></li>
-                            <li><a href="#">Manufacturing</a></li>
-                            <li><a href="#">Marketing &amp; Advertising</a></li>
-                            <li><a href="#">Medical &amp; Dental</a></li>
-                            <li><a href="#">Mobile App</a></li>
-                            <li><a href="#">Mortgage</a></li>
-                            <li><a href="#">Movies &amp; TV</a></li>
-                            <li><a href="#">Music &amp; Audio</a></li>
-                            <li><a href="#">News &amp; Media</a></li>
-                            <li><a href="#">Non-Profit &amp; Community</a></li>
-                            <li><a href="#">Oil and Gas</a></li>
-                            <li><a href="#">Outdoor &amp; Adventure</a></li>
-                            <li><a href="#">Payment</a></li>
-                            <li><a href="#">Payroll</a></li>
-                            <li><a href="#">Pets</a></li>
-                            <li><a href="#">Pharma</a></li>
-                            <li><a href="#">Photography</a></li>
-                            <li><a href="#">Podcast</a></li>
-                            <li><a href="#">Politics/Government</a></li>
-                            <li><a href="#">Professional Services</a></li>
-                            <li><a href="#">Property Management</a></li>
-                            <li><a href="#">Real Estate</a></li>
-                            <li><a href="#">Recruitment &amp; Staffing</a></li>
-                            <li><a href="#">Restaurants</a></li>
-                            <li><a href="#">Sales &amp; Marketing</a></li>
-                            <li><a href="#">Security</a></li>
-                            <li><a href="#">Senior Living and Care</a></li>
-                            <li><a href="#">Social &amp; Networking</a></li>
-                            <li><a href="#">Solar Power and Green Energy</a></li>
-                            <li><a href="#">Something Else</a></li>
-                            <li><a href="#">Spas &amp; Salons</a></li>
-                            <li><a href="#">Sports</a></li>
-                            <li><a href="#">Startup Incubator</a></li>
-                            <li><a href="#">Storage</a></li>
-                            <li><a href="#">Tech, Internet, Software</a></li>
-                            <li><a href="#">Transportation</a></li>
-                            <li><a href="#">Travel &amp; Hotel</a></li>
-                            <li><a href="#">Tutoring &amp; Test Prep</a></li>
-                            <li><a href="#">Venture Capital &amp; Investment</a></li>
-                            <li><a href="#">Video, Books &amp; Magazines</a></li>
-                            <li><a href="#">Virtual Reality (VR) and Augmented Reality
-                                (AR) Company</a></li>
-                            <li><a href="#">Vitamins and Supplements</a></li>
-                            <li><a href="#">Weddings &amp; Bridal</a></li>
-                        </ul>
-                    </article>
+            </div>
+                <div className={styles.secondListsWrapper}>
+                    <h3 className={styles.featuredHeader}>FEATURED CATEGORIES</h3>
+                    <div className={styles.listsContainer}>
+                        <article className={styles.linksWrapper}>
+                            <ul className={styles.linksContainer}>
+                                <li><a href="#">Agency &amp; Consulting</a></li>
+                                <li><a href="#">Analytics</a></li>
+                                <li><a href="#">Automotive</a></li>
+                                <li><a href="#">Bar &amp; Brewery</a></li>
+                                <li><a href="#">Beauty &amp; Cosmetics</a></li>
+                                <li><a href="#">Beer, Wine &amp; Spirits</a></li>
+                                <li><a href="#">Bikes Brand</a></li>
+                                <li><a href="#">Biotech</a></li>
+                                <li><a href="#">Bots &amp; AI</a></li>
+                                <li><a href="#">Cannabis &amp; CBD</a></li>
+                                <li><a href="#">Catering</a></li>
+                            </ul>
+                        </article>
+                        <article className={styles.linksWrapper}>
+                            <ul className={styles.linksContainer}>
+                                <li><a href="#">Cleaning</a></li>
+                                <li><a href="#">Co-Working Space Names</a></li>
+                                <li><a href="#">Coffee</a></li>
+                                <li><a href="#">Construction &amp; Architecture</a></li>
+                                <li><a href="#">Crowdfunding</a></li>
+                                <li><a href="#">Cryptocurrency, Blockchain</a></li>
+                                <li><a href="#">Dating &amp; Relationship</a></li>
+                                <li><a href="#">Daycare</a></li>
+                                <li><a href="#">Dental</a></li>
+                                <li><a href="#">Drone</a></li>
+                                <li><a href="#">E-Commerce &amp; Retail</a></li>
+                            </ul>
+                        </article>
+                        <article className={styles.linksWrapper}>
+                            <ul className={styles.linksContainer}>
+                                <li><a href="#">Education &amp; Training</a></li>
+                                <li><a href="#">Entertainment &amp; Arts</a></li>
+                                <li><a href="#">Event Planning &amp; Services</a></li>
+                                <li><a href="#">Events &amp; Conferences</a></li>
+                                <li><a href="#">Fashion &amp; Clothing</a></li>
+                                <li><a href="#">Finance</a></li>
+                                <li><a href="#">Fintech (Finance Technology)</a></li>
+                                <li><a href="#">Fitness &amp; Gym</a></li>
+                                <li><a href="#">Food &amp; Drink</a></li>
+                                <li><a href="#">Food Delivery &amp; Meal Kits</a></li>
+                                <li><a href="#">Food Truck</a></li>
+                            </ul>
+                        </article>
+                        <article className={styles.linksWrapper}>
+                            <ul className={styles.linksContainer}>
+                                <li><a href="#">Footwear</a></li>
+                                <li><a href="#">Furniture &amp; Home Furnishings</a></li>
+                                <li><a href="#">Games &amp; Recreational</a></li>
+                                <li><a href="#">Gaming</a></li>
+                                <li><a href="#">Green &amp; Organic</a></li>
+                                <li><a href="#">Health &amp; Wellness</a></li>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Home &amp; Garden</a></li>
+                                <li><a href="#">Insurance</a></li>
+                                <li><a href="#">Interior Design</a></li>
+                                <li><a href="#">Internet of Things (IOT)</a></li>
+                                <li><a href="#">Jewelry</a></li>
+                                <li><a href="#">Kids &amp; Baby</a></li>
+                                <li><a href="#">Landscaping</a></li>
+                                <li><a href="#">Legal, Attorney, Law</a></li>
+                                <li><a href="#">Life Coach / Motivational</a></li>
+                                <li><a href="#">Manufacturing</a></li>
+                                <li><a href="#">Marketing &amp; Advertising</a></li>
+                                <li><a href="#">Medical &amp; Dental</a></li>
+                                <li><a href="#">Mobile App</a></li>
+                                <li><a href="#">Mortgage</a></li>
+                                <li><a href="#">Movies &amp; TV</a></li>
+                                <li><a href="#">Music &amp; Audio</a></li>
+                                <li><a href="#">News &amp; Media</a></li>
+                                <li><a href="#">Non-Profit &amp; Community</a></li>
+                                <li><a href="#">Oil and Gas</a></li>
+                                <li><a href="#">Outdoor &amp; Adventure</a></li>
+                                <li><a href="#">Payment</a></li>
+                                <li><a href="#">Payroll</a></li>
+                                <li><a href="#">Pets</a></li>
+                                <li><a href="#">Pharma</a></li>
+                                <li><a href="#">Photography</a></li>
+                                <li><a href="#">Podcast</a></li>
+                                <li><a href="#">Politics/Government</a></li>
+                                <li><a href="#">Professional Services</a></li>
+                                <li><a href="#">Property Management</a></li>
+                                <li><a href="#">Real Estate</a></li>
+                                <li><a href="#">Recruitment &amp; Staffing</a></li>
+                                <li><a href="#">Restaurants</a></li>
+                                <li><a href="#">Sales &amp; Marketing</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Senior Living and Care</a></li>
+                                <li><a href="#">Social &amp; Networking</a></li>
+                                <li><a href="#">Solar Power and Green Energy</a></li>
+                                <li><a href="#">Something Else</a></li>
+                                <li><a href="#">Spas &amp; Salons</a></li>
+                                <li><a href="#">Sports</a></li>
+                                <li><a href="#">Startup Incubator</a></li>
+                                <li><a href="#">Storage</a></li>
+                                <li><a href="#">Tech, Internet, Software</a></li>
+                                <li><a href="#">Transportation</a></li>
+                                <li><a href="#">Travel &amp; Hotel</a></li>
+                                <li><a href="#">Tutoring &amp; Test Prep</a></li>
+                                <li><a href="#">Venture Capital &amp; Investment</a></li>
+                                <li><a href="#">Video, Books &amp; Magazines</a></li>
+                                <li><a href="#">Virtual Reality (VR) and Augmented Reality
+                                    (AR) Company</a></li>
+                                <li><a href="#">Vitamins and Supplements</a></li>
+                                <li><a href="#">Weddings &amp; Bridal</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <ul className={styles.squadReview}>
+                        <li><a href="https://www.shopperapproved.com/reviews/squadhelp.com/" rel="nofollow" target="shopperapproved">
+                            squadhelp.com has a Shopper Approved rating of 4.9/5 based on 2684 ratings and reviews.
+                        </a></li>
+                    </ul>
                 </div>
-                <ul className={styles.squadReview}>
-                    <li><a href="https://www.shopperapproved.com/reviews/squadhelp.com/" rel="nofollow" target="shopperapproved">
-                        squadhelp.com has a Shopper Approved rating of 4.9/5 based on 2684 ratings and reviews.
-                    </a></li>
-                </ul>
         </section>
     );
 };
