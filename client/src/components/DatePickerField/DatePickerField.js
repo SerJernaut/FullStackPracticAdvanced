f5a5dbae15758fa3f2a4285a49927f19a79927e8
serjernaut <serjernaut@yandex.ru>
2020-06-05 16:58:55 +0300

    Added valid/invalidValue validation styles for DatePickerField.
    Added transition on input hover.

--- client/src/components/DatePickerField/DatePickerField.js
+++ client/src/components/DatePickerField/DatePickerField.js
@@ -2,14 +2,21 @@ import React from "react";
 import DatePicker from "react-datepicker/es";
 import {useField, useFormikContext} from "formik";
 import "react-datepicker/dist/react-datepicker.css";
+import classNames from "classnames";
+import styles from './DatePickerField.module.sass'
 
-export const DatePickerField = ({ ...props }) => {
+export const DatePickerField = ({ ...props}) => {
     const { setFieldValue } = useFormikContext();
-    const [field] = useField(props);
+    const [field, meta] = useField(props);
+    const fieldClassName = classNames({
+        [styles.validValue]: meta.touched && !meta.error,
+        [styles.invalidValue]: meta.touched && meta.error
+    } );
     return (
         <DatePicker
             {...field}
             {...props}
+            className={fieldClassName}
             selected={(field.value && new Date(field.value)) || null}
             onChange={val => {
                 setFieldValue(field.name, val);
