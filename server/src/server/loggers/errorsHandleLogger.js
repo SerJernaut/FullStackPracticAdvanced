74a453037c46586828b8692926e187fb22431a97
serjernaut <serjernaut@yandex.ru>
2020-06-22 00:53:37 +0300

    Optimized logger:
    Deleted unneeded if...else chains. Now logger works even if json file contains mistake.

--- server/src/server/loggers/errorsHandleLogger.js
+++ server/src/server/loggers/errorsHandleLogger.js
@@ -1,11 +1,30 @@
 const path = require('path');
-const {promises: {readFile, writeFile, appendFile, mkdir}} = require('fs');
-const {exists} = require('../utils/promisifiedFunctions')
+const {promises: {readFile, appendFile, writeFile}} = require('fs');
+
+const writeLogFile = async (path, data) => {
+    await writeFile(path, JSON.stringify(data, null, 2));
+}
+
+const parseJsonAndAppendLogsToFile = async (json, path, errorData) => {
+    try{
+        let newLogs = [];
+        const jsonExistingLogs = JSON.parse(json);
+        if (Array.isArray(jsonExistingLogs)) {
+            newLogs = [...jsonExistingLogs]
+        }
+        newLogs.push(errorData);
+        await writeLogFile(path, newLogs)
+    }
+    catch(e) {
+        await writeLogFile(path, [errorData]);
+    }
+}
+
+const absolutePathToLogsDir = path.resolve(__dirname, '../logs');
+const absolutePathToLogsFile = path.resolve(absolutePathToLogsDir, 'errorLogs.json');
 
 module.exports = async (err, req, res, next) => {
     const {message, code, stack} = err;
-    const absolutePathToLogsDir = path.resolve(__dirname, '../logs');
-    const absolutePathToLogsFile = path.resolve(absolutePathToLogsDir, 'errorLogs.json');
     const errorData = {
         message: message,
         time: Date.now(),
@@ -13,27 +32,13 @@ module.exports = async (err, req, res, next) => {
         stackTrace: stack
     }
     try {
-        if (await exists(absolutePathToLogsDir)) {
-            if (await exists(absolutePathToLogsFile)) {
-                const data = await readFile(absolutePathToLogsFile);
-                if (data) {
-                    const jsonExistingLogs = JSON.parse(data);
-                    if (Array.isArray(jsonExistingLogs)) {
-                        jsonExistingLogs.push(errorData);
-                        await writeFile(absolutePathToLogsFile, JSON.stringify(jsonExistingLogs, null, 2));
-                    }
-                }
-            }
-            else {
-                await appendFile(absolutePathToLogsFile, JSON.stringify([errorData], null, 2));
-            }
-        } else {
-            await mkdir(absolutePathToLogsDir);
-            if (await exists(absolutePathToLogsDir))
-                await appendFile(absolutePathToLogsFile, JSON.stringify([errorData], null, 2));
-        }
-        next(err)
+        const oldLogs = await readFile(absolutePathToLogsFile);
+        await parseJsonAndAppendLogsToFile(oldLogs, absolutePathToLogsFile, errorData)
+        next(err);
     } catch (e) {
+        if (e.code === 'ENOENT') {
+            await appendFile(absolutePathToLogsFile, JSON.stringify([errorData], null, 2));
+        }
         next(e);
     }
 }
